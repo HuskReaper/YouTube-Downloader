446ea37ae8918dea44b9b16c9e5f258e32808f25
Angel Fernández <104192451+HuskReaper@users.noreply.github.com>
2023-03-06 09:56:06 -0600

    Added Basic Code Structure

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 # Imports #
-from pytube import YouTube as yt
+from pytube import YouTube
+from colorama import Fore, Back, Style
+import os, time
+
+# Variables #
+link = ""
+type = ""
+video = YouTube(link).streams.first().download()
+app_closed = False
+
+# Functions #
+def downloader():
+    if type == "Video":
+        video.streams.filter(progressive=True, file_extension = 'mp4')
+
+# Code #
+while not app_closed:
+    os.sytem("clear")
+    print(f"{Fore.LIGHTWHITE_EX}YouTube Downloader")
+    print(Style.RESET_ALL)
+    link = input("Element Link > ").strip()
+    type = input("Type > ").strip().capitalize()
+
