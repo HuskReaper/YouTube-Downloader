60b5b64a7ae832ef759145a474239db880357f05
Angel Fernandez <huskreaper007@gmail.com>
2024-02-22 21:26:24 -0600

    Updated README / App Code

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# YouTube Downloader
+# Project Description
 
-This is a simple youtube downloader using python, this is a personal project, it still has a lot of bugs and needs fixes.
+> Current Version: 1.01
+
+This is a simple youtube downloader using python. <br>
+All libraries used will be in the "utilities" section.
+
+## Utilities
+
+|   Requirements  |  Version  |
+| --------- | --------- |
+| Tkinter TCK/TCl | 8.6 |
+| Pytube3   |   9.6.4   |
+|   Python  |3.6 (Or up)|
+
+---
+
+[Pytube3 Github](https://github.com/get-pytube/pytube3)
--- main.py
+++ main.py
@@ -1,58 +1,56 @@
-# Imports #
+### NOTES ###
+'''
+This is a YouTube downloader using pytube3, tkinter, and customtkinter.
+Usage of new library is required as pytube is deprecated.
+The library pytube3 has many bugs, so expect to encounter server-side errors.
+Please read README.md
+'''
+### IMPORTS ###
 import tkinter, customtkinter
 from pytube import YouTube
 
-# Functions #
-def download_video():
-  finishLabel.configure(text="Downloading...", text_color="white") # Sets indicator label to a "default" state if the button is clicked.
-
-  try:
-    link = link_input.get()
-    ytObject = YouTube(link)
-    link_label.configure(text=ytObject.title, text_color='white')
-    finishLabel.configure(text="Downloaded", text_color='green')
-
-    ytObject = ytObject.streams.get_highest_resolution().download() # Downloads the video with highest res.
-
-  except Exception as error:
-    print(f"There has been an error: {error}") # Prints error.
-    finishLabel.configure(text="Error Downloading Object") # Sets label to indicate an error.
-  
-def download_audio():
-  finishLabel.configure(text="Downloading...", text_color="white")
-
-  try:
-    link = link_input.get()
-    ytObject = YouTube(link)
-    link_label.configure(text=ytObject.title, text_color='white')
-    finishLabel.configure(text="Downloaded", text_color='green')
-
-    ytObject = ytObject.streams.get_audio_only().download()
-
-  except Exception as error:
-    print(f"There has been an error: {error}")
-    finishLabel.configure(text="Error Downloading Object")
-
-# Tkinter Settings #
+### TKINTER SETTINGS ###
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("blue")
 
 app = customtkinter.CTk()
-app.geometry("720x480") # Sets window borders.
+app.geometry("600x400") # Sets window borders.
 app.title("YouTube Downloader with Python") # Sets window title.
 
-link_label = customtkinter.CTkLabel(app, text="Insert YouTube Link", font=("arial", 25))
-link_input = customtkinter.CTkEntry(app, width=350, height=40)
-link_label.pack(padx=10, pady=10)
-link_input.pack()
+link_label = customtkinter.CTkLabel(app, text="Insert YouTube Link", font=("arial", 25)); link_label.pack(padx=10, pady=10) # Main label.
+link_input = customtkinter.CTkEntry(app, width=350, height=40); link_input.pack()
+status_label = customtkinter.CTkLabel(app, text=""); status_label.pack() # Status label.
 
-finishLabel = customtkinter.CTkLabel(app, text="")
-finishLabel.pack()
+# Functions #
+def download_video():
+  status_label.configure(text="Downloading...", text_color="white")
+  
+  try:
+    link = link_input.get()
+    ytObject = YouTube(link)
+    link_label.configure(text=ytObject.title, text_color='white')
+    ytObject = ytObject.streams.get_highest_resolution().download() # Downloads the video with highest res.
+    YouTube.register_on_complete_callback(status_label.configure(text="Downloaded", text_color='green'))
 
-video_button = customtkinter.CTkButton(app, text="Download Video", command=download_video)
-audio_button = customtkinter.CTkButton(app, text="Download Audio", command=download_audio)
-video_button.pack(pady=10)
-audio_button.pack(pady=10)
+  except Exception as error:
+    status_label.configure(text="Error Downloading Object") # Sets label to indicate an error.
+    print(f"There has been an error: {error}") # Prints error.
+  
+def download_audio():
+  status_label.configure(text="Downloading...", text_color="white")
+
+  try:
+    link = link_input.get()
+    ytObject = YouTube(link)
+    link_label.configure(text=ytObject.title, text_color='white')
+    ytObject = ytObject.streams.get_audio_only().download()
+    YouTube.register_on_complete_callback(status_label.configure(text="Downloaded", text_color='green'))
+
+  except Exception as error:
+    status_label.configure(text="Error Downloading Object")
+    print(f"There has been an error: {error}")
 
 # Code #
+video_button = customtkinter.CTkButton(app, text="Download Video", command=download_video); video_button.pack(padx=10, pady=10)
+audio_button = customtkinter.CTkButton(app, text="Download Audio", command=download_audio); audio_button.pack()
 app.mainloop()
